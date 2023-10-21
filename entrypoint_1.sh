760b6169053f9d861667a8864a34c3fe41c12574
ralph isenmann <ralph.isenmann@hs-offenburg.de>
2023-10-21 09:41:04 +0200

    add resources for wf-api; add a entrypoint script

--- entrypoint_1.sh
+++ entrypoint_1.sh
@@ -18,3 +18,11 @@ ls -l "${data_dir}" | tail -n +2 >> "${DESTINATION}"
 echo ""
 echo "__ LIST FILE CONTENT OF: ${data_dir}/* __" >> "${DESTINATION}"
 cat "${data_dir}"/* >> "${DESTINATION}"
+
+data_file="/data/input_testfile"
+if [ -e "$data_file" ]
+then
+    echo >> "${DESTINATION}"
+    echo "__ CONTENT OF: ${data_file} __" >> "${DESTINATION}"
+    cat "$data_file" >> "${DESTINATION}"
+fi
